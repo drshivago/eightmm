566eccaa302f1ef1083653f649bb0032abbe847f
drshivago <moessner2015@gmail.com>
2018-08-20 03:00:33 -0400

    moved things to jupyter

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 #pyhon
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, video.fx
 
-clip = VideoFileClip()
+clip = VideoFileClip('movietest.mp4')
+
+#make new video
+x = 0
+for x in
+#loop take x screen from clip until clip end
+#  fl_image(image_func(get_frame(t)),apply_to[])
+#write videofile
+    #have screen be displayed for 3 frames
+    #x = x + 3
+
+#export video file
